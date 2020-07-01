2c7cb9bdfb3b46d1266f906411c64c190e366500
vinothsmart <s.vinothkanna90@gmail.com>
2020-07-01 11:23:12 +0530

    using react element created

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hi i'm a React App</h1>
-    </div>
-  );
+  // return (
+  //   <div className="App">
+  //     <h1>Hi i'm a React App</h1>
+  //   </div>
+  // );
+  return React.createElement("div", null, "", "h1", "Hi i'm a React App");
 }
 
 export default App;
