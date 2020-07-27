540b936dcd03f0a93f3084a7313fcf885c570118
vinothsmart <s.vinothkanna90@gmail.com>
2020-07-27 12:33:29 +0530

    using useState in Comp

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Person from "./Person/Person";
 
 function App() {
+  const [state, setState] = useState({
+    persons: [
+      { name: "Vinoth", age: 30 },
+      { name: "Ajitha", age: 29 },
+      { name: "Vignesh", age: 28 },
+    ],
+  });
   return (
     <div className="App">
       <h1>Hi i'm a React App</h1>
       <p>This is really working!</p>
-      <Person name="Vinoth" age="30" />
-      <Person name="Ajitha" age="29">
+      <Person name={state.persons[0].name} age={state.persons[0].age} />
+      <Person name={state.persons[1].name} age={state.persons[1].age}>
         My Hobbies : Learning New Things
       </Person>
-      <Person name="Vignesh" age="28" />
+      <Person name={state.persons[2].name} age={state.persons[2].age} />
     </div>
   );
   // return React.createElement(
