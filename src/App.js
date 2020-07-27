6c00d63d0cc0eaf46eb23c58c329332177058a20
vinothsmart <s.vinothkanna90@gmail.com>
2020-07-27 11:59:00 +0530

    Person comp created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import Person from "./Person/Person";
 
 function App() {
   return (
     <div className="App">
       <h1>Hi i'm a React App</h1>
       <p>This is really working!</p>
+      <Person />
     </div>
   );
   // return React.createElement(
