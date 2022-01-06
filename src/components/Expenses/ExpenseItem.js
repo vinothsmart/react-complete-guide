7020aa445f07fc106bda7a67f16956d050489a77
vinothsmart <s.vinothkanna90@gmail.com>
2022-01-06 15:02:29 +0530

    using useState

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,20 @@
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = ({ date, title, amount }) => {
+  const [compTitle, setCompTitle] = useState(title);
+
   const handleClick = useCallback(() => {
-    console.log("clicked");
+    setCompTitle("Updated!");
   }, []);
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{compTitle}</h2>
         <div className="expense-item__price">{amount}</div>
         <button onClick={handleClick}>Change Title</button>
       </div>
