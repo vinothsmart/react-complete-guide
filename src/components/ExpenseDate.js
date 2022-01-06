4f3e0fbd655a6964a8a19a66a323b4e2f59f25ff
vinothsmart <s.vinothkanna90@gmail.com>
2022-01-06 12:05:13 +0530

    creaed expense date

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ExpenseDate = (props) => {
+  const month = props.date.toLocaleString("en-Us", { month: "long" });
+  const day = props.date.toLocaleString("en-Us", { day: "2-digit" });
+  const year = props.date.getFullYear();
+  return (
+    <div>
+      <div>{month}</div>
+      <div>{year}</div>
+      <div>{day}</div>
+    </div>
+  );
+};
+
+export default ExpenseDate;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,11 @@
 import React from "react";
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  const month = props.date.toLocaleString("en-Us", { month: "long" });
-  const day = props.date.toLocaleString("en-Us", { day: "2-digit" });
-  const year = props.date.getFullYear();
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__descrption">
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
