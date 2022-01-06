9fb2f04c1801fbb7ac59f99e07a993d420594926
vinothsmart <s.vinothkanna90@gmail.com>
2022-01-06 18:30:51 +0530

    rm uncontroller comp

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,22 +1,14 @@
-import { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = ({ date, title, amount }) => {
-  const [compTitle, setCompTitle] = useState(title);
-
-  const handleClick = () => {
-    setCompTitle("Updated!");
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{compTitle}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{amount}</div>
-        <button onClick={handleClick}>Change Title</button>
       </div>
     </Card>
   );
