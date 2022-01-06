b7c88eabfe316e48698a025f26ddf43826f92d0b
vinothsmart <s.vinothkanna90@gmail.com>
2022-01-06 11:05:38 +0530

    Date splited

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,9 +2,16 @@ import React from "react";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const month = props.date.toLocaleString("en-Us", { month: "long" });
+  const day = props.date.toLocaleString("en-Us", { day: "2-digit" });
+  const year = props.date.getFullYear();
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__descrption">
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
