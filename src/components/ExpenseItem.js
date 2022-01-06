1b422c827e7047a689682eaebf3da023462375f6
vinothsmart <s.vinothkanna90@gmail.com>
2022-01-06 06:46:03 +0530

    updated expense comp

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const ExpenseItem = () => {
-  return <h2>Expense item!</h2>;
+  return (
+    <>
+      <div>Jan 17th 2022</div>
+      <div>
+        <h2>Bike Insurance</h2>
+        <div>Rs 1045</div>
+      </div>
+    </>
+  );
 };
 
 export default ExpenseItem;
