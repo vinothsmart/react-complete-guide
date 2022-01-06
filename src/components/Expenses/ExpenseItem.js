e26142e85b29bd3aec101cffe56218cb07c4166e
vinothsmart <s.vinothkanna90@gmail.com>
2022-01-06 14:46:20 +0530

    onClick event

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,20 @@
+import { useCallback } from "react";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = ({ date, title, amount }) => {
+  const handleClick = useCallback(() => {
+    console.log("clicked");
+  }, []);
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">{amount}</div>
+        <button onClick={handleClick}>Change Title</button>
       </div>
     </Card>
   );
