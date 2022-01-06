5c2a6e177117fb1a5725741ea3badacec1327357
vinothsmart <s.vinothkanna90@gmail.com>
2022-01-06 14:14:45 +0530

    children props className

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,16 @@
+import Card from "./Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = ({ date, title, amount }) => {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">{amount}</div>
       </div>
-    </div>
+    </Card>
   );
 };
 
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,13 +1,14 @@
+import Card from "./Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 
 const Expenses = ({ items }) => {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       {items.map((item) => (
         <ExpenseItem key={item.id} {...item} />
       ))}
-    </div>
+    </Card>
   );
 };
 
