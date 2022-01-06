038b09d2129eb37e5edb0f61274d96d972baec8e
vinothsmart <s.vinothkanna90@gmail.com>
2022-01-06 10:52:25 +0530

    added const

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,12 +2,15 @@ import React from "react";
 import "./ExpenseItem.css";
 
 const ExpenseItem = () => {
+  const expenseDate = new Date(2021, 0, 17);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = "Rs. 3200";
   return (
     <div className="expense-item">
-      <div>Jan 17th 2022</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__descrption">
-        <h2>Bike Insurance</h2>
-        <div className="expense-item__price">Rs 1045</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">{expenseAmount}</div>
       </div>
     </div>
   );
