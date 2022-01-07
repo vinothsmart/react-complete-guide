a6b8fe8a9c60985773f3adfa422b7c1c35c5c30b
vinothsmart <s.vinothkanna90@gmail.com>
2022-01-07 16:34:13 +0530

    deleted old project

--- src/components/UI/Card.js
+++
@@ -1,8 +0,0 @@
-import "./Card.css";
-
-const Card = ({ children, className }) => {
-  const classes = "card " + className;
-  return <div className={classes}>{children}</div>;
-};
-
-export default Card;
