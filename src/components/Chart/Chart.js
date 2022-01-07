5ec63357ba8526db23cef35ed6f8ce3477b4a231
vinothsmart <s.vinothkanna90@gmail.com>
2022-01-07 15:13:27 +0530

    adding chart

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,19 @@
+import "./Chart.css";
+import ChartBar from "./ChartBar";
+
+const Chart = ({ dataPoints }) => {
+  return (
+    <div className="chart">
+      {dataPoints.map((dataPoint) => (
+        <ChartBar
+          key={dataPoint.label}
+          value={dataPoint.value}
+          maxValue={null}
+          label={dataPoint.label}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Chart;
