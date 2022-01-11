6b71cee97413578460e26f1231803216f9bc9476
vinothsmart <s.vinothkanna90@gmail.com>
2022-01-11 19:05:48 +0530

    AddUser Component

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,17 @@
+const AddUser = ({ username, age }) => {
+  const addUserHandler = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form onSubmit={addUserHandler}>
+      <label htmlFor="username">UserName</label>
+      <input id="username" type="text" />
+      <label htmlFor="age">Age (Years)</label>
+      <input id="age" type="number" />
+      <button type="submit">Add User</button>
+    </form>
+  );
+};
+
+export default AddUser;
