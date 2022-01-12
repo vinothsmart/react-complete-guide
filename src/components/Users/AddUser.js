31e088dd35c40c99ceb27a7219f8029f5f8d12e5
vinothsmart <s.vinothkanna90@gmail.com>
2022-01-12 17:47:00 +0530

    manging input state

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,19 +1,33 @@
+import { useState } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 
 const AddUser = ({ username, age }) => {
+  const [values, setValues] = useState({
+    username: "",
+    age: "",
+  });
+
   const addUserHandler = (e) => {
     e.preventDefault();
+    console.log(values);
+  };
+
+  const inputChangeHandler = (e) => {
+    setValues((prevValues) => ({
+      ...prevValues,
+      [e.target.id]: e.target.value,
+    }));
   };
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">UserName</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" onChange={inputChangeHandler} />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" />
+        <input id="age" type="number" onChange={inputChangeHandler} />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
