6e0bfeb348af2d58ebcedcb0df297b626add6c16
vinothsmart <s.vinothkanna90@gmail.com>
2022-03-29 12:32:23 +0530

    updated to arrow func

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
-function App() {
+const App = () => {
   const [expenses, setExpenses] = useState([
     {
       id: "e1",
@@ -32,6 +32,6 @@ function App() {
       <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
